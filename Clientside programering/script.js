bf006c09c117b299015a2ba77fcd51539beb1a78
Rasmus Frøstrup <rasm364p@edu.mercantec.dk>
2020-11-12 14:35:34 +0100

    Dagens Fremskridt
    Har fået added tabels og har fået lavet boxmodellen på tabels

--- Clientside programering/script.js
+++ Clientside programering/script.js
@@ -3,4 +3,7 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
 
-document.getElementById("showTime").innerHTML = dateTime;
+function setup(){
+
+    document.getElementById("showTime").innerHTML = dateTime;
+}
